366ff0d222b46dc307904878fc860788ba299acd
Eike Schuderer <es@initart.com>
2015-10-13 21:22:21 +0200

    using pre compiled semantic ui as workaround

--- themes/semantic-ui/elements/header_top.php
+++ themes/semantic-ui/elements/header_top.php
@@ -3,7 +3,7 @@
 <html lang="<?php echo Localization::activeLanguage()?>">
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <?php echo $html->css($view->getStylesheet('semantic.css'))?>
+    <?php echo $html->css('assets/css/semantic.css')?>
     <?php Loader::element('header_required', array('pageTitle' => isset($pageTitle) ? $pageTitle : ''));?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
